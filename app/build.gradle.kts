fdb93e510d66bc9ba44a31082d06cb9e1bd11b28
AKBARM67 <aqbarmaulana58@gmail.com>
2023-11-26 06:42:33 +0700

    token rubah1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField ("String", "BASE_URL", "\"https://api.github.com/\"")
-        buildConfigField("String", "KEY", "\"token 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\"")
+        buildConfigField("String", "KEY", "\"token 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\"")
     }
 
 
